aac93437300190442323cbc07de0860ef803a1db
elenarosskopf <elena.rosskopf@gmail.com>
2014-12-20 17:24:30 +0100

    task2 fixed?!
    task2 teils in appendix mit drin

--- R/task2plot.R
+++ R/task2plot.R
@@ -1,5 +1,5 @@
 
-data = read.csv("testtask2table.csv",skip=6, header = T)
+data = read.csv("task2_PDneighborhood-table.csv",skip=6, header = T)
 t(data)
 data = as.data.frame(data, header = T)
 
@@ -23,14 +23,13 @@ aggdata3HD <-aggregate(dataneumannHD, by=list(dataneumannHD$r), FUN=mean, na.rm=
 
 par(mfrow=c(1,3))
 
-#plot(aggdata1HD$r, aggdata1HD$freq_c, las=1,pch=18,xlab="C-B-ratio", ylab="frequency of coop") #HDm1
-#plot(aggdata3HD$r, aggdata3HD$freq_c,las=1, pch=18,  xlab="C-B-ratio", ylab="frequency of coop" ) #HDn1
-#plot(aggdata2HD$r, aggdata2HD$freq_c,las=1, pch=18, xlab="C-B-ratio", ylab="frequency of coop" ) #HDm3
-
 ggplot(aggdata1HD, aes(x = r, y=freq_c))+ geom_point() + labs(colour=NULL) + xlim(0,1)+ xlab("C-B-ratio") + ylab("Proportion of Cooperators")
 ggplot(aggdata3HD, aes(x = r, y=freq_c))+ geom_point() + labs(colour=NULL) + xlim(0,1)+ xlab("C-B-ratio") + ylab("Proportion of Cooperators")
 ggplot(aggdata2HD, aes(x = r, y=freq_c))+ geom_point() + labs(colour=NULL) + xlim(0,1)+ xlab("C-B-ratio") + ylab("Proportion of Cooperators")
 
+##############################PD################################################################
+###############################################################
+######################
 
 dataPD = data[which(data$game == '"PD"'),]
 datamoore1= dataPD[which(dataPD$neighborhood == '"moore_r=1"'),]
@@ -38,21 +37,17 @@ datamoore3= dataPD[which(dataPD$neighborhood == '"moore_r=3"'),]
 dataneumann= dataPD[which(dataPD$neighborhood == '"von neumann_r=1"'),]
 
 datamoore1$r <- datamoore1$cost / (datamoore1$benefit - datamoore1$cost)
-datamoore1$freq_c <- datamoore1$count.patches.with..strategy_current....C../ 2500
+datamoore1$freq_c <- datamoore1$count.patches.with..strategy_current....C.....count.patches
 
 datamoore3$r <- datamoore3$cost / (datamoore3$benefit - datamoore3$cost)
-datamoore3$freq_c <- datamoore3$count.patches.with..strategy_current....C../ 2500
+datamoore3$freq_c <- datamoore3$count.patches.with..strategy_current....C.....count.patches
 
 dataneumann$r <- dataneumann$cost / (dataneumann$benefit - dataneumann$cost)
-dataneumann$freq_c <- dataneumann$count.patches.with..strategy_current....C../ 2500
+dataneumann$freq_c <- dataneumann$count.patches.with..strategy_current....C.....count.patches
 
 aggdata1 <-aggregate(datamoore1, by=list(datamoore1$r), FUN=mean, na.rm=TRUE)
 aggdata2 <-aggregate(datamoore3, by=list(datamoore3$r), FUN=mean, na.rm=TRUE)
 aggdata3 <-aggregate(dataneumann, by=list(dataneumann$r), FUN=mean, na.rm=TRUE)
-
-plot(aggdata1$r, aggdata1$freq_c, las=1,pch=18,xlab="C-B-ratio",ylab="frequency of coop") #PDm1
-#plot(aggdata3$r, aggdata3$freq_c,las=1, pch=18, xlab="C-B-ratio",  xlim=c(0,1),ylab="frequency of coop" ) #PDn1
-#plot(aggdata2$r, aggdata2$freq_c,las=1, pch=18, xlab="C-B-ratio",  xlim=c(0,1),ylab="frequency of coop" ) #PDm3
 
 ggplot(aggdata1, aes(x = r, y=freq_c))+ geom_point() + labs(colour=NULL) + xlim(0,1)+ xlab("C-B-ratio") + ylab("Proportion of Cooperators")
 ggplot(aggdata3, aes(x = r, y=freq_c))+ geom_point() + labs(colour=NULL) + xlim(0,1)+ xlab("C-B-ratio") + ylab("Proportion of Cooperators")
